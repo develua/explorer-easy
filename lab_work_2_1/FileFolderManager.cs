372c819f45f8247b761f837ef49c34f10325e73b
prodan7799 <prodan7799@gmail.com>
2017-01-11 23:12:10 +0200

    first commit

---
+++ lab_work_2_1/FileFolderManager.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Runtime.InteropServices;
+
+namespace lab_work_2_1
+{
+    class FileFolderManager
+    {
+        string path;
+
+        public FileFolderManager()
+        {
+            path = "";
+        }
+
+        public void LoadDrives(ItemCollection colection)
+        {
+            colection.Clear();
+
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+                colection.Add(drive.ToString());
+        }
+
+        public void LoadFileFolder(ItemCollection colection, int index = -1)
+        {
+            if (index != -1)
+            {
+                if (colection[index].ToString() == "...")
+                {
+                    if (path.Length > 3)
+                        path = Directory.GetParent(path).FullName;
+                    else
+                    {
+                        path = "";
+                        LoadDrives(colection);
+                        return;
+                    }
+                }
+                else
+                {
+                    if (Directory.Exists(Path.Combine(path, colection[index].ToString())))
+                        path = Path.Combine(path, colection[index].ToString());
+                    else
+                        return;
+                }
+            }
+            else
+            {
+                if (path == "")
+                    return;
+            }
+
+            colection.Clear();
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            colection.Add("...");
+
+            try
+            {
+                foreach (var item in dir.GetDirectories())
+                    colection.Add(item.Name);
+
+                foreach (var item in dir.GetFiles())
+                    colection.Add(item.Name);
+            }
+            catch { }
+        }
+
+        public Task CopyFileFolder(string nameFileFolder, string selectedPath)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                if (nameFileFolder == "..." || path == "")
+                    return;
+
+                Thread.Sleep(300);
+
+                string pathCombine = Path.Combine(path, nameFileFolder);
+
+                try
+                {
+                    if (Directory.Exists(pathCombine))
+                        DirectoryCopy(pathCombine, Path.Combine(selectedPath, nameFileFolder));
+                    else
+                        File.Copy(pathCombine, Path.Combine(selectedPath, Path.Combine(selectedPath, nameFileFolder)));
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            });
+        }
+
+        public Task MoveFileFolder(string nameFileFolder, string selectedPath)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                if (nameFileFolder == "..." || path == "")
+                    return;
+
+                Thread.Sleep(300);
+
+                string pathCombine = Path.Combine(path, nameFileFolder);
+
+                try
+                {
+                    if (Directory.Exists(pathCombine))
+                    {
+                        DirectoryCopy(pathCombine, Path.Combine(selectedPath, nameFileFolder));
+                        Directory.Delete(pathCombine, true);
+                    }
+                    else
+                        File.Move(pathCombine, Path.Combine(selectedPath, nameFileFolder));
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            });
+        }
+
+        public Task DelFileFolder(string nameFileFolder)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                if (nameFileFolder == "..." || path == "")
+                    return;
+
+                Thread.Sleep(300);
+
+                string pathCombine = Path.Combine(path, nameFileFolder);
+
+                try
+                {
+                    if (Directory.Exists(pathCombine))
+                        Directory.Delete(pathCombine);
+                    else
+                        File.Delete(pathCombine);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            });
+        }
+
+        private void DirectoryCopy(string SourcePath, string DestinationPath)
+        {
+            foreach (string dirPath in Directory.GetDirectories(SourcePath, "*",
+            SearchOption.AllDirectories))
+            Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
+
+            foreach (string newPath in Directory.GetFiles(SourcePath, "*.*",
+            SearchOption.AllDirectories))
+            File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath), true);
+        }
+    }
+}
---
+++ lab_work_2_1/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using MahApps.Metro.Controls;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Collections.Generic;
+using System.Threading;
+using System;
+
+namespace lab_work_2_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        FileFolderManager manager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            manager = new FileFolderManager();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            manager.LoadDrives(listFileFolder.Items);
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            CopyMoveFileFolder(manager.CopyFileFolder);
+        }
+
+        private void MoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            CopyMoveFileFolder(manager.MoveFileFolder);
+        }
+
+        private async void DelButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(listFileFolder.SelectedItem == null)
+                return;
+
+            MessageBoxResult resultMB = MessageBox.Show("Вы уверены, что хотите произвести удаление?", "Вопрос", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (resultMB == MessageBoxResult.No)
+                return;
+
+            progressBar.Maximum = listFileFolder.SelectedItems.Count;
+
+            for (int i = 0; i < listFileFolder.SelectedItems.Count; i++)
+            {
+                await manager.DelFileFolder(listFileFolder.SelectedItems[i].ToString());
+                progressBar.Value = i + 1;
+            }
+
+            await Task.Factory.StartNew(() => Thread.Sleep(1000));
+            progressBar.Value = 0;
+            manager.LoadFileFolder(listFileFolder.Items);
+        }
+
+        private void listFileFolder_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (listFileFolder.SelectedItem != null)
+                manager.LoadFileFolder(listFileFolder.Items, listFileFolder.SelectedIndex);
+        }
+
+        private async void CopyMoveFileFolder(Func<string, string, Task> methodCopyMove)
+        {
+            if (listFileFolder.SelectedItem == null)
+                return;
+
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult resultDialog = dialog.ShowDialog();
+
+            if (resultDialog == System.Windows.Forms.DialogResult.OK)
+            {
+                progressBar.Maximum = listFileFolder.SelectedItems.Count;
+
+                for (int i = 0; i < listFileFolder.SelectedItems.Count; i++)
+                {
+                    await methodCopyMove(listFileFolder.SelectedItems[i].ToString(), dialog.SelectedPath);
+                    progressBar.Value = i + 1;
+                }
+
+                await Task.Factory.StartNew(() => Thread.Sleep(1000));
+                progressBar.Value = 0;
+                manager.LoadFileFolder(listFileFolder.Items);
+            }
+        }
+    }
+}
